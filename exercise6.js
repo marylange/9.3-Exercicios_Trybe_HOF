75e7fe42865b8d1eca5c38f9fddda2a75fece76c
Marylange <marylangess@gmail.com>
2020-03-31 10:44:50 -0300

    Exercícios HOF

---
+++ exercise6.js
@@ -0,0 +1,2 @@
+// Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array.Porém, essa modelagem
+// está baixo nível.Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
